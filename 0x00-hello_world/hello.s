f13d33404010fced4e16e47336e56afed856a82f
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-03 06:10:53 -0700

    ALX

--- 0x00-hello_world/hello.s
+++ 0x00-hello_world/hello.s
@@ -1,4 +1,5 @@
 	.file	"hello.c"
+	.intel_syntax noprefix
 	.text
 	.section	.rodata
 .LC0:
@@ -10,17 +11,17 @@ main:
 .LFB0:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	movl	$-2147483648, %esi
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
+	mov	esi, -2147483648
+	lea	rdi, .LC0[rip]
+	mov	eax, 0
 	call	printf@PLT
-	movl	$0, %eax
-	popq	%rbp
+	mov	eax, 0
+	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
