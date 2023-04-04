3e00140b2572f30425680d00209992e5932f39fb
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-04 05:51:00 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a = 'z';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a--;
+	}
+	return (0);
+}
