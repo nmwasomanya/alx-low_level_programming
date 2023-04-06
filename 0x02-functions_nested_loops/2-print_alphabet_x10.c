97eb1398b3a29c350b553297845282e562dcae46
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-06 02:04:06 -0700

    Functions, Nested Loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,22 @@
  *
  * Return - 0
  */
+
 void print_alphabet_x10(void)
 {
-int b = 0;
-while (b < 10)
-{
-char a = 'a';
-while (a <= 'z')
-{
-_putchar(a);
-a++;
+	int i = 0;
+	
+	while (i < 10)
+	{
+		char x = 'a';
+		while (x <= 'z')
+		{
+			_putchar(x);
+			x++;
+		}
+		_putchar("\n");
+		i++;
+	}
 }
-_putchar('\n');
-b++;
-}
-}
+
+
