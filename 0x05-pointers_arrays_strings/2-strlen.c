d97329b39cb848763897636f40f2fa0957ba9628
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-14 06:18:17 -0700

    Pointers, Arrays & Strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _strlen - to get the length
+ *
+ *  @s: char input
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	int count = 0;
+	
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+}
