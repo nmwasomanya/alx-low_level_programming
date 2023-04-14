60bb4aad37f5e94fb089ba29b7ca28daf564b598
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-14 04:54:19 -0700

    Pointers, Arrays & Strings

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - start here
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
