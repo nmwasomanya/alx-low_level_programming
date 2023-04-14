b6c6ecc396e2c278f66daaf256e05de06ead5cde
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-14 06:00:39 -0700

    Pointers, Arrays & Strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ * 
+ * @a: first argument
+ * @b: second argument
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
