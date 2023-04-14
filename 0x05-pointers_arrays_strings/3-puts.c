53aa164211eb551cf7171b10bb21c97f963d009a
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-14 06:39:41 -0700

    Pointers, Arrays & Strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,11 +9,11 @@
 
 void _puts(char *str)
 {
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        printf("%c", str[i]);
-        i++;
-    }
-    printf("\n");
+       	int i = 0;
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i++;
+	}
+	printf("\n");
 }
