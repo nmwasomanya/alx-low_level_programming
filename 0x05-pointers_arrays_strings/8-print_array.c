3e9f01a07bb050129848f565331edae29d197da4
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-15 03:03:37 -0700

    Pointers, Arrays & Strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h"
+
+/**
+ * print_array - prints an array n times
+ *
+ * @a: input array
+ * n: number values
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+	       	printf("%d, ", a[i]);
+	       	i++;
+	}
+	putchar('\n');
+}
