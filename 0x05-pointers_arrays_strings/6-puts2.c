5ed8f12113c75e8bfd33169d1ba7b10353ca4b32
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-15 01:45:13 -0700

    Pointers, Arrays & Strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - printts only values with even numbered indexes
+ *
+ * @str: input string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] % 2) == 0)
+		{
+			_putchar(str[i];
+		}
+		i++;
+	}
+}
