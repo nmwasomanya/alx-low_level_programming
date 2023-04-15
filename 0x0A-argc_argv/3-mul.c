b4fff93fbbe997ac78ea09b07d48c149d4693311
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-15 08:34:03 -0700

    argv, argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+		int sum = num1 * num2;
+		printf("%d\n", sum);
+
+		return (0);
+	}
+}
