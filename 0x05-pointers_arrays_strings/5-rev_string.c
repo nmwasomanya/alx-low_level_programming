64eec5a511aa669880b07729feda00d604a337ac
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-15 01:35:02 -0700

    Pointers, Arrays & Strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: variable holding the string
+ * Return: s
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	{
+		char temp = s[i];
+
+		s[i] = s[j];
+		s[j] = temp;
+	}
+	return (s);
+}
