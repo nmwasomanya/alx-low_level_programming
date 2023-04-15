35dfddfc1631e6fb3ff28ebbd4a8df76bc5b0bbf
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-15 08:15:11 -0700

    argv, argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of the arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
