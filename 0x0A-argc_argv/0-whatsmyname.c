f39e0ffac4c031f7322ad70713d8f61248592fff
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-15 07:59:39 -0700

    argv, argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	printf("\n");
+
+	return (0);
+}
