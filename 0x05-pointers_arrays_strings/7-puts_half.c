5bd3ed9eb43dae1fd1f26d84b6e9a03190d36f04
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-15 02:16:37 -0700

    Pointers, Arrays & Strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints the last half
+ *
+ * @str: input string
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	if ((len % 2) == 0)
+	{
+		int i = len / 2;
+
+		while (str[i] != '\0')
+		{
+			putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		int i = (len - 1) / 2;
+		
+		while(str[i] != '\0')
+		{
+			putchar(str[i];
+			i++;
+		}
+	}
+	putchar('\n')
+}
+
