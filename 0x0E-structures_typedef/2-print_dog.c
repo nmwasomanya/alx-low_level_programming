e4de5c5f1e76ea669cc4f9bb88ccd12510058246
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-17 04:40:26 -0700

    Structures aand Typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints te dog struct
+ * @d: dog struct
+ * Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d -> name : "(null)");
+		printf("Age: %f\n", (d->age) ? d -> age : "(null)");
+		printf("Owner: %s\n", (d->owner) ? d -> owner : "(null)");
+	}
+}
