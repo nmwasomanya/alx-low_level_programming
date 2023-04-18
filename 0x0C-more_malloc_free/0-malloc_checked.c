ba537e668f714e1e11ac47eba4186ed34ff29e54
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-04-18 03:47:58 -0700

    ALX

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * malloc_checked - creates memory allocation
+ * @b: input b
+ * Return: returns c
+ */
 
 void *malloc_checked(unsigned int b)
 {
 	void *c;
+
 	c = malloc(b);
 
 	if (c == NULL)
 		exit(98);
-	
 	return (c);
 }
