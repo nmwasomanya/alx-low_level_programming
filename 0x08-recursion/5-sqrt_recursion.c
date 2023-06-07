180bfecb11fcd9cd93b4edee953313e54040417b
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-06-07 02:19:54 -0700

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** int _sqrt_recursion - finds square root
+ * @n: input number
+ *
+ * Return: sqrt or -1 if no sqrt found
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 1)
+	{
+		return (-1);
+	}
+	return (0);
+}
