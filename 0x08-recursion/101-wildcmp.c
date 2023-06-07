2f795cf722459ffc76d4740b7ecab14205a81801
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-06-07 09:24:42 -0700

    Recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if identical or 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2)
+	{
+		return (0);
+	}
+	else if (strlen(s1) != strlen(s2))
+	{
+		return (0);
+	}
+	else if (*s1 == '\0' || *s2 == '\0')
+	{
+		return (1);
+	}
+	return (wildcmp(s1 + 1, s2 + 1));
+}
+
