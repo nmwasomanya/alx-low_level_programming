71d6f4d1769426109cfae028f8abc0dd8ed13304
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-06-09 05:23:21 -0700

    argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,17 +11,33 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv;
+	int amount = atoi(argv[1]);
+	int cents;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	else if (atoi(argv[1]) < 0)
+	else if (amount < 0)
 	{
 		printf("%d\n", 0);
 	}
+
+	else if ((amount % 10) == 0)
+	{
+		cents = amount / 10;
+		printf("%d\n", cents);
+	}
+	
+	
+
+
+
+
+
+
+
 
 	return (0);
 }
