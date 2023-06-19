018cd2c00716160a8e7ebc6f000eed6208215f19
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-06-19 04:49:27 -0700

    alx

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,7 +4,10 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %f\n", (d->age != 0.0) ? d->age : 0.0 );
-	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age != 0.0) ? d->age : 0.0 );
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	}
 }
