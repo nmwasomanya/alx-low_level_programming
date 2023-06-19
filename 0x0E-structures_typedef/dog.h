a9d0d1ffc4fc2cafae2c2e20b6abc14ef5cea699
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-06-19 04:30:55 -0700

    alx

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,11 +20,11 @@ struct dog
 /**
  * dog_t - Typedef for dog structure
  */
-
+/**
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
+*/
 #endif
