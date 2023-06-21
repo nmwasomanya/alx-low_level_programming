c9258b0e1981a6efd7c3d2df5a6e9cb768b20076
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-06-21 03:45:49 -0700

    Fixed int index program

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
@@ -31,4 +31,3 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	return (-1);
 }
-	
