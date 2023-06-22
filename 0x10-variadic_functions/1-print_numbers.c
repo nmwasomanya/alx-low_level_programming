acb2e17e94f38a4c2338c3f6d9f7e623dfa38c40
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-06-22 04:07:55 -0700

    print numbers first commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - ...
+ * @separator: ...
+ * @n: ...
+ * Return: ..
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	unsigned int i;
+	int x;
+
+	va_start(args, n);
+
+	if (n == 0)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		printf("%d", x);
+		if (separator != NULL)
+		{
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
+
